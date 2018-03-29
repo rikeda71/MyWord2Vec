7753bde4427ced3fdb85744ce73039ac3f02b71a
Ryuya Ikeda <rikeda71@gmail.com>
2018-03-29 20:30:21 +0900

    Edit: train in test

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import model
 with open("sample.txt", "r") as f:
     sentences = [line.replace("\n", "").split(" ") for line in f.readlines()]
 
-vocab = list(set(sum(sentences, [])))
-w2v = model.Word2Vec(vocab, sentences, 100)
-w2v.train()
+w2v = model.Skipgram(sentences, 100)
+# w2v = model.Cbow(sentences, 100)
+w2v.train(batch_size=8)
+# w2v.load()
+w2v.similar_word("this")
+print(w2v.degree_of_similarity("this", "that"))
