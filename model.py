a8fbcd45aef70d835138298272c451afa181975a
Ryuya Ikeda <rikeda71@gmail.com>
2018-03-29 17:05:11 +0900

    Add: forward method in word2vec classes

--- model.py
+++ model.py
@@ -1,16 +1,19 @@
 import numpy as np
+from abc import ABCMeta, abstractmethod
+from layers import *
 
 
-class Word2Vec(object):
+class Word2Vec(metaclass=ABCMeta):
     """
     word2vecを実装するクラス
     """
 
-    def __init__(self, vocab: list, sentences: list, dimention: int=100):
+    def __init__(self, vocab: list, sentences: list, dimention: int=100, window: int=3):
         """
         vocab     : list of words in text data
         sentences : list of sentences(separated by word)
         dimention : size of words matrix row
+        window    : size of window
         ex)
         vocab = ["I", "You", "play", ...]
         sentences = [["I", "have", "a", "pen"], ["You", "play", "baseball"], ...]
@@ -18,15 +21,30 @@ class Word2Vec(object):
 
         self.vocab = vocab
         self.sentences = sentences
+        self.window = window
         # make one hot(1-of-k) vector
         self.one_hot = np.mat(np.identity(len(vocab)))
         # weight matrix. it is used in hidden-layer
         # ex) hidden_w * one_hot[0].T
-        self.hidden_w = np.mat(np.random.rand(dimention, len(vocab)) -
-                               np.full((dimention, len(vocab)), 0.5))
+        hidden_w = np.mat(np.random.rand(dimention, len(vocab)) -
+                          np.full((dimention, len(vocab)), 0.5))
         # ex) output_w * (hidden_w * one_hot[0])
-        self.output_w = np.mat(np.random.rand(len(vocab), dimention) -
-                               np.full((len(vocab), dimention), 0.5))
+        output_w = np.mat(np.random.rand(len(vocab), dimention) -
+                          np.full((len(vocab), dimention), 0.5))
+        self.__set__(hidden_w, output_w)
+        self.wv = hidden_w
+
+    @abstractmethod
+    def __set__(self, hidden_w, output_w):
+        pass
+
+    @abstractmethod
+    def __forward__(self, contexts, word):
+        pass
+
+    @abstractmethod
+    def __backward__(self, cotexts, word):
+        pass
 
     def train(self, algorithm="skipgram", window: int=3, eta: float=0.1):
         """
@@ -38,3 +56,37 @@ class Word2Vec(object):
         """
 
         self.train_skipgram(window, eta)
+
+
+class Skipgram(Word2Vec):
+    """
+    skipgram model
+    """
+
+    def __set__(self, hidden_w, output_w):
+        self.inLayer = MatrixMultiply(hidden_w)
+        self.outLayer = MatrixMultiply(output_w)
+        self.lossLayer = [SoftMaxWithError() for i in range(self.window * 2)]
+
+    def __forward__(self, contexts, word):
+        h = self.inLayer.forward(self.onehot(word))
+        s = self.outLayer.forward(h)
+        loss = sum([l.forward(s, self.one_hot(i)) for i, l in zip(range(self.window * 2), self.lossLayer)])
+        return loss
+
+
+class Cbow(Word2Vec):
+    """
+    cbow model
+    """
+
+    def __set__(self, hidden_w, output_w):
+        self.inLayer = [MatrixMultiply(hidden_w) for i in ragen(len(self.window))]
+        self.outLayer = MatrixMultiply(output_w)
+        self.lossLayer = SoftMaxWithError()
+
+    def __forward__(self, contexts, word):
+        h = sum([l.forward(s, self.one_hot(i)) for i, l in zip(range(self.window * 2), self.inLayer)]) / len(self.inLayer)
+        s = self.outLayer(h)
+        loss = self.lossLayer.forward(s, self.one_hot(word))
+        return loss
